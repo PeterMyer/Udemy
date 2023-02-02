10652282b9c67268be5acd351ce8367d3073d026
Peter Myer <peter.sr.myer@gmail.com>
2023-02-02 14:28:43 -0600

    set up sign in and sign out header

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,16 @@
+import buildClient from "../api/build-client";
 
-const LandingPage = () => {
+const LandingPage = ({ currentUser }) => {
     return (
-        <div>
-            <h1>Landing Page</h1>
-        </div>
+        currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
     )
+}
+
+LandingPage.getInitialProps = async(context) => {
+    const client = buildClient(context)
+    const { data } = await client.get('/api/users/currentuser')
+
+    return data
 }
 
 export default LandingPage;
