82f3466ddc6500580bf1ba2c057af9b1613e809a
Peter Myer <peter.sr.myer@gmail.com>
2023-02-08 18:54:26 -0600

    exported middleware logic to common library and imported back into auth

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,9 @@
 import express ,{ Request, Response } from 'express'
 import { body } from 'express-validator'
-
 import { Password } from '../services/password'
 import jwt from 'jsonwebtoken'
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@pm_tickets/common'
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router= express.Router()
 
