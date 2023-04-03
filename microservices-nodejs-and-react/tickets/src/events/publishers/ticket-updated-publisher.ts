7b19cd8f2ce547dee2c74d4ab456b3ce0930fcb2
Peter Myer <peter.sr.myer@gmail.com>
2023-04-02 20:03:54 -0500

    renamed directory to better reflect content

---
+++ microservices-nodejs-and-react/tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,6 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@pm_tickets/common'
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent>{
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
+
