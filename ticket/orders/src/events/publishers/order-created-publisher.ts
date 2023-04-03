6b3102efd12de5046faf4077580c1fa9f234d10a
Peter Myer <peter.sr.myer@gmail.com>
2023-04-02 20:05:03 -0500

    renamed ticket project to better reflect content

--- ticket/orders/src/events/publishers/order-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, OrderCreatedEvent } from "@pm_tickets/common";
-
-export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent>{
-    subject: Subjects.OrderCreated = Subjects.OrderCreated;
-}
--- ticket/tickets/src/events/publishers/ticket-updated-publisher.ts
+++
@@ -1,6 +0,0 @@
-import { Publisher, Subjects, TicketUpdatedEvent } from '@pm_tickets/common'
-
-export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent>{
-    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-}
-
