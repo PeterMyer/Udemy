99a48d29f723580b8618878847b92e632c6f9e77
Peter Myer <peter.sr.myer@gmail.com>
2023-04-05 21:11:23 -0500

    wired up expirationcompletelistener to orders index

--- microservices-nodejs-and-react/orders/src/events/listeners/expiration-complete-listener.ts
+++ microservices-nodejs-and-react/orders/src/events/listeners/expiration-complete-listener.ts
@@ -4,7 +4,7 @@ import { queueGroupName } from "./queue-group-name";
 import { Order } from '../../models/order'
 import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
-export class ExpirationCompletePublisher extends Listener<ExpirationCompleteEvent>{
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent>{
     queueGroupName = queueGroupName;
     subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
 
