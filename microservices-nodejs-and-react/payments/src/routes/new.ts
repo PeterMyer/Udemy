b0cf86c6c3a01bca93369d412757bfd0f8edc1d7
Peter Myer <peter.sr.myer@gmail.com>
2023-04-20 21:53:54 -0500

    update charge and payment buisness logic in payment new.ts allows for the creation and record of stripe payment

--- microservices-nodejs-and-react/payments/src/routes/new.ts
+++ microservices-nodejs-and-react/payments/src/routes/new.ts
@@ -10,6 +10,9 @@ import {
 } from '@pm_tickets/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe'
+import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payments-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -38,13 +41,24 @@ async (req: Request, res: Response) => {
         throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
         currency: 'usd',
         amount: order.price * 100,
         source: token,
     })
 
-    res.send({ success: true })
+    const payment = Payment.build({
+        orderId,
+        stripeId: charge.id
+    })
+    await payment.save();
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripeId: payment.stripeId
+    })
+
+    res.status(201).send({ id: payment.id })
 
 
 });
