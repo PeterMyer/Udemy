1302897880adde66579929632b935234e60ee7ea
Peter Myer <peter.sr.myer@gmail.com>
2023-04-23 10:52:20 -0500

    expanded order view page to allow payment and count down time until order is no longer reserved

--- microservices-nodejs-and-react/client/pages/tickets/[ticketId].js
+++ microservices-nodejs-and-react/client/pages/tickets/[ticketId].js
@@ -1,4 +1,5 @@
 import useRequest from "../../hooks/use-request";
+import Router from 'next/router';
 
 const TicketShow = ({ ticket }) => {
     const {doRequest, errors } = useRequest({
@@ -7,14 +8,14 @@ const TicketShow = ({ ticket }) => {
         body: {
             ticketId: ticket.id
         },
-        onSuccess: (order) => console.log(order)
+        onSuccess: (order) => Router.push('/orders/[orderId]', `/orders/${order.id}`)
     })
 
     return <div>
         <h1>{ ticket.title }</h1>
         <h4>Price: { ticket.price } </h4>
         {errors}
-        <button onClick = {doRequest} className = "btn btn-primary">Purchase</button>
+        <button onClick = {() => doRequest()} className = "btn btn-primary">Purchase</button>
     </div>
 }
 
