14b2d86decfe5e4dc5f4964a034e7c2920b0d76c
Peter Myer <peter.sr.myer@gmail.com>
2023-04-27 22:54:01 -0500

    set up graphql-react skeleton directory

---
+++ GraphQL-React/server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
+
+app.listen(3000, () => {
+  console.log('Server is running on Port 3000');
+});
