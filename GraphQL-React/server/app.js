9ed2e7bfd1d39173f61d9c21b6cda490094462ca
Peter Myer <peter.sr.myer@gmail.com>
2023-05-08 18:09:11 -0500

    updated app.js and connected to mongoDB changed app.js and schema to ES6 module added mongoose mongoDB connection

--- GraphQL-React/server/app.js
+++ GraphQL-React/server/app.js
@@ -1,8 +1,18 @@
-const express = require('express');
+import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
-const schema = require('./schema/schema');
+import mongoose from 'mongoose';
+import { schema } from './schema/schema.js';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.vre2xqk.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(CONNECTION_URL);
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+});
 
 app.all('/graphql', createHandler({ schema }));
 
--- GraphQL-React/server/schema/schema.js
+++ GraphQL-React/server/schema/schema.js
@@ -1,4 +1,4 @@
-const graphql = require('graphql');
+import graphql from 'graphql';
 
 const {
   GraphQLObjectType,
@@ -56,6 +56,6 @@ const RootQuery = new GraphQLObjectType({
   }),
 });
 
-module.exports = newGraphQLSchema({
+export const schema = new GraphQLSchema({
   query: RootQuery,
 });
