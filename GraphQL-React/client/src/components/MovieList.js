df3f53f4a192cfb78b8667481505c4ac98391ae9
Peter Myer <peter.sr.myer@gmail.com>
2023-06-01 19:23:27 -0500

    added function to reder <li> of movie names

--- GraphQL-React/client/src/components/MovieList.js
+++ GraphQL-React/client/src/components/MovieList.js
@@ -12,13 +12,17 @@ const GET_MOVIES_QUERY = gql`
 
 function MovieList() {
   const { loading, data, error } = useQuery(GET_MOVIES_QUERY);
+
   if (loading) return <p>Loading...</p>;
-  console.log(data);
+
+  const renderMovies = (data) => {
+    return data?.movies?.map((movie) => {
+      return <li key={movie.id}>{movie.name}</li>;
+    });
+  };
   return (
     <div className="App">
-      <ul>
-        <li>Movie</li>
-      </ul>
+      <ul>{renderMovies()}</ul>
     </div>
   );
 }
