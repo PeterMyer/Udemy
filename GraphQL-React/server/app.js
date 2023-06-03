2210ad746314c538cbd070ddebcfb8b953662e71
Peter Myer <peter.sr.myer@gmail.com>
2023-06-02 19:06:41 -0500

    switched graphql-http out for graphql-yoga and added cors origin options

--- GraphQL-React/server/app.js
+++ GraphQL-React/server/app.js
@@ -1,14 +1,21 @@
 import express from 'express';
-import { createHandler } from 'graphql-http/lib/use/express';
+// import { createHandler } from 'graphql-http/lib/use/express';
+import { createYoga } from 'graphql-yoga';
 import mongoose from 'mongoose';
 import { schema } from './schema/schema.js';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config();
+
 const app = express();
 
-app.use(cors);
+const corsOptions = {
+  origin: ['http://localhost:3000/', 'http://localhost:3001/'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.vre2xqk.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(CONNECTION_URL);
@@ -16,7 +23,13 @@ mongoose.connection.once('open', () => {
   console.log('Connected to DB');
 });
 
-app.all('/graphql', createHandler({ schema }));
+const yoga = createYoga({
+  schema,
+  graphiql: true,
+  cors: false,
+});
+
+app.use('/graphql', yoga);
 
 app.listen(3001, () => {
   console.log('Server is running on Port 3001');
